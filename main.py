3a6327c9d3891e1110c82bd80c61ed940b9f71dc
zyuf287 <zyuf287@aucklanduni.ac.nz>
2022-08-04 16:45:44 +0800

    Updated console output

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Hello cs235 students. Yes I am one of them, but maybe not awesome")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
